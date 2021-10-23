dee77f7e0a52dfecb7a540f9335b79751e4225b9
reuven9632 <reuven9632@gmail.com>
2021-10-23 13:59:09 +0300

    Thread Deamon

---
+++ src/main/java/com/example/demo/AtestCodeAndFitches/declarativeProgrammingStream/derry/MyThread/Ex_Thread_Deamon.java
@@ -0,0 +1,37 @@
+package com.example.demo.AtestCodeAndFitches.declarativeProgrammingStream.derry.MyThread;
+
+public class Ex_Thread_Deamon {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread_Ex9 = new Thread(new Thread_1());
+        thread_Ex9.start();
+        thread_Ex9.join();
+
+        Thread thread_TelephoneCall = new Thread(new TelephoneCall());
+        thread_TelephoneCall.setDaemon(true);
+        thread_TelephoneCall.start();
+
+        System.out.println("main wait 2 sec");
+        Thread.sleep(2000);
+        System.out.println("main end work!");
+    }
+
+
+
+
+
+
+
+}
+
+class Thread_1 implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Thread srarts!!");
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Thread ends " + Thread.currentThread().getName());
+    }
+}
